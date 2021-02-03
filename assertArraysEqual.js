1f4b9084ecb23f758b9c5641ec5045190efec314
gavinswan <gavtswan@gmail.com>
2021-02-03 03:33:44 +0000

    Comparing Arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let first = '';
+  let second = '';
+  for (let i = 0; i < array1.length; i++) {
+    first = array1[i];
+    for (let j = 0; j < array2.length; j++) {
+      second = array2[i];
+    }
+  }
+  return first === second;
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
