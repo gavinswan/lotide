a3253f40439c6eea9c9e756ae57e239cfa3afff7
gavinswan <gavtswan@gmail.com>
2021-02-05 00:58:18 +0000

    using a callback in map

---
+++ map.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let first = '';
+  let second = '';
+  for (let i = 0; i < array1.length; i++) {
+    first = array1[i];
+    for (let j = 0; j < array2.length; j++) {
+      second = array2[j];
+    }
+  }
+  return first === second;
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+console.log(assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]));
+
