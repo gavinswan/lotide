9e3aae498a81c9de38e52f9ed6e890ca852a6118
gavinswan <gavtswan@gmail.com>
2021-02-08 22:05:27 +0000

    refactoring lotide

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(array1, array2) {
   let first = '';
@@ -18,10 +11,4 @@ const eqArrays = function(array1, array2) {
   return first === second;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3,]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+module.exports = eqArrays;
